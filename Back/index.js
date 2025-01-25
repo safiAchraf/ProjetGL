37b9c30561fef1f7d48eaa8a2f4454d518ab7a59
SafiAchraf <a_safi@estin.dz>
2025-01-25 23:50:06 +0100

    cors shit

--- Back/index.js
+++ Back/index.js
@@ -25,7 +25,7 @@ const app = express();
 app.use(
   cors({
     credentials: true,
-    origin: "https://projet-gl-jet.vercel.app",
+    origin: "https://projet-gl-jet.vercel.app"
     	// process.env.NODE_ENV == "development"
     	// 	? "http://localhost:5173"
     	// 	: "someurl",

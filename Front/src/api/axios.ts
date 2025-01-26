8302ce204ce9732f98d1fabfe11c14c42a6d2278
SafiAchraf <a_safi@estin.dz>
2025-01-26 12:50:08 +0100

    axiox link

--- Front/src/api/axios.ts
+++ Front/src/api/axios.ts
@@ -1,10 +1,10 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig } from "axios";
 
-// const DEBUG_URL = "http://localhost:3000";
+const DEBUG_URL = "http://localhost:3000";
 const RELEASE_URL = "https://gl-backend-32cy.onrender.com/";
-
+const BASE_URL = process.env.NODE_ENV === "development" ? DEBUG_URL : RELEASE_URL;
 export const api: AxiosInstance = axios.create({
-  baseURL: RELEASE_URL,
+  baseURL: BASE_URL,
   timeout: 10000,
   withCredentials: true,
   headers: {

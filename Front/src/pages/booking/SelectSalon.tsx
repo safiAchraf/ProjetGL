de497a98b79c1206da75e9036cc56441a2f486f5
0xA1M <45073092+0xA1M@users.noreply.github.com>
2025-01-26 20:29:28 +0100

    Minor fixes

--- Front/src/pages/booking/SelectSalon.tsx
+++ Front/src/pages/booking/SelectSalon.tsx
@@ -113,7 +113,7 @@ const SelectSalon = () => {
     }
 
     return (
-      <section className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
+      <section className="w-full mx-auto px-4 sm:px-6 lg:px-8 py-6">
         {salons.length > 0 ? (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {salons.map((salon) => (
